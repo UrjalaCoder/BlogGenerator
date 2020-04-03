27e2cccb126a38983117a238ac211edd6ef526de
Tuomas Myllymäki <myllymakituomas1998@gmail.com>
2020-04-03 23:14:20 +0300

    Happy with the parser for now

--- data_parser.py
+++ data_parser.py
@@ -3,18 +3,16 @@ import xml.dom.minidom as minidom
 import re
 from bs4 import BeautifulSoup
 
-# Illegal XML characters
-RE_XML_ILLEGAL = u'([\u0000-\u0008\u000b-\u000c\u000e-\u001f\ufffe-\uffff])' + \
-                 u'|' + \
-                 u'([%s-%s][^%s-%s])|([^%s-%s][%s-%s])|([%s-%s]$)|(^[%s-%s])' % \
-                  (chr(0xd800),chr(0xdbff),chr(0xdc00),chr(0xdfff),
-                   chr(0xd800),chr(0xdbff),chr(0xdc00),chr(0xdfff),
-                   chr(0xd800),chr(0xdbff),chr(0xdc00),chr(0xdfff))
+def remove_punctuation(raw_string):
+    punctuation_re = r'[.,"\/#?!$%\^&\*;:{}=\-_`~()]'
+    return re.sub(re.compile(punctuation_re), "", raw_string).lower()
 
-def load_xml(filename):
+def parse_posts(filename):
     filename = f"raw_data/{filename}.xml"
     raw_data = open(filename).read()
-    soup = BeautifulSoup(raw_data).find_all("post")
-    print(len(soup))
+    posts_raw_strings = BeautifulSoup(raw_data, "xml").find_all("post")
+    posts = [re.sub(r'</?post>', " ", str(s)).strip() for s in posts_raw_strings]
+    posts = list(map(remove_punctuation, posts))
+    print(posts[0:10])
 
-load_xml("1270648.female.23.indUnk.Scorpio")
+parse_posts("1270648.female.23.indUnk.Scorpio")
