fdc6eb53a2fda83c3b3a57cdc58faab645d1f6f8
Tuomas Myllymäki <myllymakituomas1998@gmail.com>
2020-04-04 00:16:46 +0300

    Finished parser, TODO: save numerical data and the dictionary object to a file. Start building the training set generator

--- data_parser.py
+++ data_parser.py
@@ -3,6 +3,7 @@ import xml.dom.minidom as minidom
 import re
 from bs4 import BeautifulSoup
 from spellchecker import SpellChecker
+import glob, os
 
 spellchecker = SpellChecker(language="en", distance=1)
 
@@ -10,14 +11,26 @@ def remove_punctuation(raw_string):
     punctuation_re = r'[.,"\/#?!$%\^&\*;:{}=\-_`~()]'
     return re.sub(re.compile(punctuation_re), "", raw_string).lower()
 
-def parse_posts(filename):
-    filename = f"raw_data/{filename}.xml"
+def parse_posts(filename, dictionary_dict):
+    filename = f"raw_data/{filename}"
     raw_data = open(filename).read()
     posts_raw_strings = BeautifulSoup(raw_data, "xml").find_all("post")
     posts = [re.sub(r'</?post>', " ", str(s)).strip() for s in posts_raw_strings]
     posts = list(map(remove_punctuation, posts))
-    words = parse_to_words(posts)
-    print(words)
+    posts = parse_to_words(posts)
+    post_numericals = []
+    for post in posts:
+        post_number_list = []
+        for word in post:
+            index = None
+            if word in dictionary_dict:
+                index = dictionary_dict[word]
+            else:
+                index = len(dictionary_dict.keys()) + 1
+                dictionary_dict[word] = index
+            post_number_list.append(index)
+        post_numericals.append(post_number_list)
+    return post_numericals
 
 def check_words(formatted_words):
     checked = []
@@ -25,14 +38,25 @@ def check_words(formatted_words):
         checked.append(spellchecker.correction(word))
     return checked
 
+def check_is_english(word):
+    try:
+        word.encode(encoding='utf-8').decode('ascii')
+        return True
+    except UnicodeDecodeError:
+        return False
+    else:
+        return True
+
 def parse_to_words(raw_posts):
     words = []
     counter = 0
     for post in raw_posts:
+        skip_next = False
         post_words = post.split(" ")
         formatted_words = []
-        skip_next = False
         for word in post_words:
+            if check_is_english(word) is not True:
+                break
             if word == "i":
                 formatted_words.append("I")
             elif len(word) < 1:
@@ -47,8 +71,26 @@ def parse_to_words(raw_posts):
                 formatted_words.append(word)
         checked = check_words(formatted_words)
         words.append(checked)
-        print(f"COUNTER: {counter}")
         counter += 1
     return words
 
-parse_posts("1270648.female.23.indUnk.Scorpio")
+def parse_all():
+    BASE_DATA_DIR = "./raw_data"
+    dictionary_dict = {}
+    post_numericals = []
+    total_post_count = 0
+    for f in glob.glob(f"{BASE_DATA_DIR}/*.xml"):
+        if total_post_count > 10000:
+            break
+        b_name = os.path.basename(f)
+        print(b_name)
+        try:
+            numericals = parse_posts(b_name, dictionary_dict)
+            total_post_count += len(numericals)
+            # print(numericals)
+            print(f"POSTS: {total_post_count}")
+            post_numericals.append(numericals)
+        except UnicodeError:
+            print(f"FAILED {b_name}")
+            pass
+parse_all()
