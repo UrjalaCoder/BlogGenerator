61c8ea0699cb243945c7a1b3771fbfe1f40dae47
Tuomas Myllymäki <myllymakituomas1998@gmail.com>
2020-04-04 19:00:59 +0300

    Dataset generation

--- dataset_generator.py
+++ dataset_generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+from data_parser import load_post_data
 
 # Returns a 2d array of hotvectors for each word in line
 def make_to_hotvector(line, word_list_size):
@@ -11,18 +12,20 @@ def make_to_hotvector(line, word_list_size):
 def get_training_example(post, word_list_size, feature_count=4):
     training_example = []
     for i in range(0, len(post) - feature_count + 1):
-        line = post[i:(i + feature_count)]
-        hotvectors = make_to_hotvector(line, word_list_size)
-        training_example.append(hotvectors)
+        label = post[(i + feature_count - 1)]
+        hotvectors = make_to_hotvector(label, word_list_size)
+        label = hotvectors[-1]
+        training_data = post[i:(i + feature_count - 1)]
+        training_example.append((np.array(training_data), label))
     return training_example
 
-def generate_training_dataset(posts, word_list):
+def generate_training_dataset(posts, word_list, feature_count=4):
     dataset = []
-    feature_count = 4
     word_list_size = len(word_list)
     for post in posts:
-        post_set = get_training_example(post, word_list_size, feature_count=4)
+        post_set = get_training_example(post, word_list_size, feature_count=feature_count)
         dataset += (post_set)
+    dataset = np.array(dataset)
     return dataset
 
 def test_make_to_hotvektor():
@@ -46,8 +49,21 @@ def test_generate_training_dataset():
     dataset = generate_training_dataset(posts, word_list)
     print(dataset)
 
-def test_for_real_data():
+def generate_dataset(input_filename="data", filename="dataset", cutoff=10, feature_count=4):
+    try:
+        posts, word_list = load_post_data(filename=input_filename)
+        dataset = generate_training_dataset(posts[0:cutoff], word_list, feature_count=feature_count)
+        path = f"datasets/{filename}"
+        np.save(path, dataset)
+        print(f"Saved {len(dataset)} elements in {path}")
+    except Exception:
+        print(f"failed to generate dataset with args: {input_filename} {filename} {cutoff} {feature_count}")
 
+def test_for_real_data(cutoff=10):
+    posts, keys = load_post_data(filename="data")
+    word_list = keys
+    feature_count = 4
+    dataset = generate_training_dataset(posts[0:cutoff], word_list, feature_count=feature_count)
 
 # Testing
-test_generate_training_dataset()
+generate_dataset()
